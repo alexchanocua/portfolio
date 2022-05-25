b666b2bb1d092c199ff5ffb1f1e745024d4b9944
alexchanocua <chanocuaalejandro@gmail.com>
2022-05-24 23:17:06 -0700

    changed css and structure of modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -5,35 +5,39 @@ const Modal = ( {closeModal, modalInfo} ) => {
 
 
   return (
-    <div className="modalBackground">
-      <div className="modalContainer">
-        <div className="titleCloseBtn">
-          <button onClick={() => {closeModal(false)}}>&times;</button>
-        </div>
-        
-        <div className="title">
-          <h2>{modalInfo.title}</h2>
-        </div>
-
-        <div className="body">
-          <div className="modalImage">
-            <img src={modalInfo.image} alt="image" />
+    <div className="modal__background">
+        <div className="container modal__container">
+          <div className="titleCloseBtn">
+            <button onClick={() => {closeModal(false)}}>&times;</button>
           </div>
-
-          <div className="description">
-            <p>{modalInfo.description}</p>
           
-          <h3>Technologies Used</h3>
-          <ul>
-            {modalInfo.tech.map(name => {
-              return(<li>{name}</li>)
-            })}
-          </ul>
+          {/* <div className="title">
+            <h2>{modalInfo.title}</h2>
+          </div> */}
+
+          <div className="body">
+            <div className="modalImage">
+              <img src={modalInfo.image} alt="image" />
+            </div>
+
+            <div className="description">
+              <div className="summary">
+                <h2>Description</h2>
+                <p>{modalInfo.description}</p>
+              </div>
+              <div className="tech">
+                <h2>Technologies Used</h2>
+                <ul>
+                  {modalInfo.tech.map(name => {
+                    return(<li>{name}</li>)
+                  })}
+                </ul>
+            </div>
+            </div>
+
             
           </div>
-          
         </div>
-      </div>
     </div>
   )
 };
